63760de793f7b3093ed629dff4b09b2c3ad7b227
andrejarboe <andrejarboe@gmail.com>
2018-01-04 11:54:42 -0500

    add items to server

--- items_db.sql
+++ items_db.sql
@@ -5,6 +5,20 @@ USE items_db;
 CREATE TABLE bids(
 	id INT(11) AUTO_INCREMENT NOT NULL,
     name VARCHAR(100),
-    current_bid FLOAT(10),
+    currentBid FLOAT(10) DEFAULT 0,
     PRIMARY KEY(id)
 );
+
+INSERT INTO bids(name) VALUE ("Playstation");
+INSERT INTO bids(name) VALUE ("Mac Book Pro");
+INSERT INTO bids(name) VALUE ("Xbox One S");
+INSERT INTO bids(name) VALUE ("iPhone X");
+INSERT INTO bids(name) VALUE ("4k TV");
+INSERT INTO bids(name) VALUE ("Roku");
+INSERT INTO bids(name) VALUE ("Amazon Alexa");
+INSERT INTO bids(name) VALUE ("Tesla Model S");
+INSERT INTO bids(name) VALUE ("Microsofth Surface Pro 4");
+INSERT INTO bids(name) VALUE ("HTC Vive");
+INSERT INTO bids(name) VALUE ("iPad Pro");
+
+SELECT * FROM bids;
